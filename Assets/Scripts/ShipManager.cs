d4e8a5a934f32419fe29d8bd2e781e38d456e316
Michael Goff <mhgoff01@louisville.edu>
2026-03-31 03:54:56 -0400

    Finally Fixed

--- Assets/Scripts/ShipManager.cs
+++ Assets/Scripts/ShipManager.cs
@@ -30,15 +30,11 @@ public class ShipManager : MonoBehaviour
     [SerializeField] private bool placementLocked = false; // bool check to lock placement when all ships are placed
     [SerializeField] private bool isActiveBoard = true; // checks if this board is active
 
-<<<<<<< HEAD
     private bool CanEditShips() // extra check
     {
         return isActiveBoard && !placementLocked;
     }
 
-=======
-    private TMP_Text[] texts;
->>>>>>> parent of 89d099d (Added Turn Locking so that once player 1 has placed all ships they can press ready and player 2 can place their ships which after can lead to combat phase)
     private enum ShipTypes
     {
         Two = 0,
@@ -76,16 +72,7 @@ public class ShipManager : MonoBehaviour
     private Vector3 _bCollider;
     private Vector3 _cCollider;
 
-<<<<<<< HEAD
     // prevent overlap
-=======
-    //for setting collision detections
-    private Vector3 bcollider;
-    private Vector3 ccollider;
-    //check how many total are left
-    private int _shipcount = 0;
-    //prevent overlap
->>>>>>> parent of 89d099d (Added Turn Locking so that once player 1 has placed all ships they can press ready and player 2 can place their ships which after can lead to combat phase)
     public LayerMask overlap;
 
     // making a number to add onto names
@@ -99,8 +86,8 @@ public class ShipManager : MonoBehaviour
     private void Start()
     {
         // find network
-        //_network = GameObject.FindWithTag("NetworkManager")?.GetComponent<SessionManager>();
-        //if (!_network) Debug.LogError("Unable to find NetworkManager!");
+        // _network = GameObject.FindWithTag("NetworkManager")?.GetComponent<SessionManager>();
+        // if (!_network) Debug.LogError("Unable to find NetworkManager!");
 
         // get ship type manager
         _shipTypeManager = GetComponent<ShipTypeManager>();
@@ -129,7 +116,6 @@ public class ShipManager : MonoBehaviour
         // maximum number
         _shipRations = new Dictionary<int, int>
         {
-<<<<<<< HEAD
             [(int)ShipTypes.Two] = 1,
             [(int)ShipTypes.Three] = 1,
             [(int)ShipTypes.Four] = 1,
@@ -142,34 +128,9 @@ public class ShipManager : MonoBehaviour
         _shipText = new Dictionary<int, Label>();
 
         foreach (int shipType in _shipRations.Keys)
-=======
-            [ShipTypes.Two] = 4,
-            [ShipTypes.Three] = 3,
-            [ShipTypes.Four] = 2,
-            [ShipTypes.Five] = 1
-        };
-
-        //set up starting UI with number of available ships
-        texts[0].text = (_shipRations[ShipTypes.Two]) + "";
-        texts[1].text = (_shipRations[ShipTypes.Three]) + "";
-        texts[2].text = (_shipRations[ShipTypes.Four]) + "";
-        texts[3].text = (_shipRations[ShipTypes.Five]) + "";
-
-
-        // init ship objects dict with null values
-        _shipObjects = new Dictionary<ShipTypes, List<ShipView>>();
-        foreach ((ShipTypes shipType, int count) in _shipRations)
->>>>>>> parent of 89d099d (Added Turn Locking so that once player 1 has placed all ships they can press ready and player 2 can place their ships which after can lead to combat phase)
         {
-            List<ShipView> objects = new(count);
-            for (int i = 0; i < count; i++)
-            {
-                objects.Add(null);
-                _shipcount = _shipcount + 1;
-            }
-            _shipObjects[shipType] = objects;
+            _shipObjects[shipType] = new List<ShipView>();
         }
-        Debug.Log("shipcount: " + _shipcount);
 
         // create button ui
         BuildShipButtons();
@@ -189,7 +150,6 @@ public class ShipManager : MonoBehaviour
         UpdateButtons();
     }
 
-<<<<<<< HEAD
     private void BuildShipButtons()
     {
         if (uiDoc == null || shipPlaceButton == null)
@@ -256,42 +216,8 @@ public class ShipManager : MonoBehaviour
             Destroy(ship);
             UpdateButtons();
             return;
-=======
-   /* public void Protect()
-    {
-        for (int i = 0; i < _shipObjects.Count; i++)
-        {
-            ChosenShip(i);
-            foreach (var ship in _shipObjects[_selectedShip])
-            {
-                ship.transform.parent = null;
-                DontDestroyOnLoad(ship);
-            }
         }
-    }*/
 
-    private void Redo(GameObject Ship)
-    {
-        //Debug.Log("got this far");
-        ChosenShip(Ship.GetComponent<LineShipView>().shipLength - 2);
-        //Debug.Log(_shipObjects[_selectedShip]);
-        //_shipObjects[_selectedShip].RemoveAt(Ship.GetComponent<LineShipView>().index);
-        for (int i = 0; i < _shipObjects[_selectedShip].Count; i++)
-        {
-            /*
-            Debug.Log("found ship name: " + Ship.name);
-            Debug.Log("listed ship name: " + _shipObjects[_selectedShip][i].gameObject.name);
-            */
-            if (_shipObjects[_selectedShip][i].gameObject.name == Ship.name)
-            {
-                _shipObjects[_selectedShip].RemoveAt(i);
-                Destroy(Ship);
-                texts[(int)_selectedShip].text = SelectedRemaining() + "";
-                return;
-            }
-            //Debug.Log(_shipObjects[_selectedShip]);
->>>>>>> parent of 89d099d (Added Turn Locking so that once player 1 has placed all ships they can press ready and player 2 can place their ships which after can lead to combat phase)
-        }
         Debug.Log("couldn't find ship");
     }
 
@@ -333,26 +259,14 @@ public class ShipManager : MonoBehaviour
     {
         // returns the number of selected ships remaining
         int startingRation = _shipRations[_selectedShip];
-<<<<<<< HEAD
         int numberPlaced = _shipObjects[_selectedShip].Count;
-=======
-        int numberPlaced = _shipObjects[_selectedShip].Count(ship => ship);
-        if (startingRation - numberPlaced == 0)
-        {
-            texts[(int)_selectedShip].gameObject.transform.parent.GetComponent<Button>().interactable = false;
-        }
-        else
-        {
-            texts[(int)_selectedShip].gameObject.transform.parent.GetComponent<Button>().interactable = true;
-        }
->>>>>>> parent of 89d099d (Added Turn Locking so that once player 1 has placed all ships they can press ready and player 2 can place their ships which after can lead to combat phase)
         return startingRation - numberPlaced;
     }
 
     private bool GhostValid()
     {
         // TODO - don't allow placing on top of another ship
-        return SelectedRemaining() > 0 && // check if ship size available
+        return SelectedRemaining() > 0 &&
                _ghost != null &&
                _ghost.HasValidPlacement(spaceBuilder.GetSize());
     }
@@ -370,7 +284,7 @@ public class ShipManager : MonoBehaviour
 
     private void RotateShip()
     {
-        if (!isActiveBoard) return;
+        if (!CanEditShips()) return;
 
         // alters the rotation axis
         if (!_ghost) return;
@@ -392,7 +306,7 @@ public class ShipManager : MonoBehaviour
 
     private void CycleShip()
     {
-        if (!isActiveBoard) return;
+        if (!CanEditShips()) return;
 
         // called to cycle through ship types
         _selectedShip = _shipTypeManager.CycleShip(_selectedShip);
@@ -420,12 +334,7 @@ public class ShipManager : MonoBehaviour
 
     private void HandleCursorMoved()
     {
-<<<<<<< HEAD
         if (!CanEditShips()) return;
-=======
-        if (!isActiveBoard) return;
-
->>>>>>> parent of 89d099d (Added Turn Locking so that once player 1 has placed all ships they can press ready and player 2 can place their ships which after can lead to combat phase)
         if (!_placing) return;
 
         if (_ghost)
@@ -440,11 +349,7 @@ public class ShipManager : MonoBehaviour
             ReinstantiateGhost();
         }
     }
-<<<<<<< HEAD
 
-=======
-    private bool free = true;
->>>>>>> parent of 89d099d (Added Turn Locking so that once player 1 has placed all ships they can press ready and player 2 can place their ships which after can lead to combat phase)
     private void Update()
     {
         if ((Mouse.current.rightButton.isPressed) & free)
@@ -453,12 +358,8 @@ public class ShipManager : MonoBehaviour
             free = false;
             StartCoroutine(Delay(0.5f));
         }
-<<<<<<< HEAD
 
         if (CanEditShips() && Mouse.current.scroll.ReadValue().y != 0)
-=======
-        if (Mouse.current.scroll.ReadValue().y != 0)
->>>>>>> parent of 89d099d (Added Turn Locking so that once player 1 has placed all ships they can press ready and player 2 can place their ships which after can lead to combat phase)
         {
             ReinstantiateGhost();
         }
@@ -493,14 +394,7 @@ public class ShipManager : MonoBehaviour
 
     private void PlaceShip()
     {
-<<<<<<< HEAD
         if (!CanEditShips()) return; // checks if valid to place ships
-=======
-        if (!isActiveBoard) return;
-
-        // places a ship where the ghost ship is
-
->>>>>>> parent of 89d099d (Added Turn Locking so that once player 1 has placed all ships they can press ready and player 2 can place their ships which after can lead to combat phase)
         if (!GhostValid()) return; // ghost ship required
         if (SelectedRemaining() == 0) return; // check if ship available
 
@@ -511,17 +405,17 @@ public class ShipManager : MonoBehaviour
         if (Mathf.Approximately(_ghost.transform.rotation.eulerAngles.y, 90))
         {
             _bCollider = new Vector3(1, 1, len);
-            _cCollider.z += (float)((len - 1) * .5);
+            _cCollider.z += (len - 1) * 0.5f;
         }
         else if (Mathf.Approximately(_ghost.transform.rotation.eulerAngles.z, 90))
         {
             _bCollider = new Vector3(1, len, 1);
-            _cCollider.y += (float)((len - 1) * .5);
+            _cCollider.y += (len - 1) * 0.5f;
         }
         else
         {
             _bCollider = new Vector3(len, 1, 1);
-            _cCollider.x += (float)((len - 1) * .5);
+            _cCollider.x += (len - 1) * 0.5f;
         }
 
         // the overlap box needs to have dimensions half that of the original, or it will be too large
@@ -533,7 +427,6 @@ public class ShipManager : MonoBehaviour
         {
             return;
         }
-<<<<<<< HEAD
 
         ShipView newShip = Instantiate(
             ObjectFromSelected(),
@@ -552,43 +445,6 @@ public class ShipManager : MonoBehaviour
 
         colliderObject.name = colliderObject.name + " " + _index;
         _index++;
-
-        // send placement to server
-        if (_network != null)
-        {
-            _network.PlaceShip(_selectedShip, ShipsPlaced(_selectedShip), newShip);
-        }
-=======
-
-        if (_shipRations[_selectedShip] - SelectedRemaining() >= _shipObjects[_selectedShip].Count)
-        {
-            _shipObjects[_selectedShip].Add(
-              Instantiate(ObjectFromSelected(),
-                  _ghost.transform.position,
-                  _ghost.transform.rotation,
-                  transform));
-        }
-        else
-        {
-            // get the dict of ship objects for the selected ship
-            // then get the index of the maximum ship ration - remaining amount
-            // then set that to a prefab placed at the ghost's location
-            Debug.Log((_shipRations[_selectedShip] - SelectedRemaining()));
-            _shipObjects[_selectedShip][_shipRations[_selectedShip] - SelectedRemaining()] =
-                Instantiate(ObjectFromSelected(),
-                    _ghost.transform.position,
-                    _ghost.transform.rotation,
-                    transform);
-        }
-        //Debug.Log("index 2: " + (_shipRations[_selectedShip] - SelectedRemaining()));
-        Debug.Log("number placed: " + ((_shipRations[_selectedShip] - SelectedRemaining()) - 1));
-        GameObject collider_object = _shipObjects[_selectedShip][(_shipRations[_selectedShip] - SelectedRemaining()) - 1].gameObject;
-        collider_object.layer = LayerMask.NameToLayer("Ship");
-        collider_object.transform.GetChild(0).gameObject.layer = LayerMask.NameToLayer("Ship");
-        collider_object.name = collider_object.name + " " + _index;
-        _index = _index + 1;
-      //  Debug.Log("listed ship name: " + _shipObjects[_selectedShip][(_shipRations[_selectedShip] - SelectedRemaining()) - 1].gameObject.name);
->>>>>>> parent of 89d099d (Added Turn Locking so that once player 1 has placed all ships they can press ready and player 2 can place their ships which after can lead to combat phase)
 
         // update UI
         UpdateButtons();
@@ -616,7 +472,6 @@ public class ShipManager : MonoBehaviour
     {
         isActiveBoard = active;
     }
-<<<<<<< HEAD
 
     public bool AllShipsPlaced() // iterates through the dict and compares how many ships are allowed to how many are placed
     {
@@ -656,6 +511,4 @@ public class ShipManager : MonoBehaviour
 
         LockPlacement();
     }
-=======
->>>>>>> parent of 89d099d (Added Turn Locking so that once player 1 has placed all ships they can press ready and player 2 can place their ships which after can lead to combat phase)
 }
--- Assets/Scripts/TurnManager.cs
+++ Assets/Scripts/TurnManager.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class TurnManager : MonoBehaviour
 {
@@ -20,14 +19,36 @@ public class TurnManager : MonoBehaviour
         SetTurn(true);
     }
 
-    private void Update()
+    public void ConfirmPlacement()
     {
-        if (Keyboard.current.tKey.wasPressedThisFrame)
+        Debug.Log(player1Turn ? "Checking Player 1 placement" : "Checking Player 2 placement");
+
+        if (player1Turn)
         {
-            SwitchTurn();
+            if (!player1ShipManager.AllShipsPlaced())
+            {
+                Debug.Log("Player 1 must place all ships before locking in.");
+                return;
+            }
+
+            player1ShipManager.LockPlacement();
+            player1Turn = false;
+            SetTurn(false);
+        }
+        else
+        {
+            if (!player2ShipManager.AllShipsPlaced())
+            {
+                Debug.Log("Player 2 must place all ships before locking in.");
+                return;
+            }
+
+            player2ShipManager.LockPlacement();
+            Debug.Log("Both players locked in. Start battle phase here.");
+
+            CombatPhase();
         }
     }
-
 
     public void SwitchTurn()
     {
@@ -44,5 +65,16 @@ public class TurnManager : MonoBehaviour
         player2Board.SetActiveBoard(!isPlayer1Turn);
         player2ShipManager.SetActiveBoard(!isPlayer1Turn);
         player2Camera.gameObject.SetActive(!isPlayer1Turn);
+    }
+
+    private void CombatPhase()
+    {
+        player1Board.SetActiveBoard(false);
+        player1ShipManager.SetActiveBoard(false);
+        player1Camera.gameObject.SetActive(false);
+
+        player2Board.SetActiveBoard(false);
+        player2ShipManager.SetActiveBoard(false);
+        player2Camera.gameObject.SetActive(false);
     }
 }
