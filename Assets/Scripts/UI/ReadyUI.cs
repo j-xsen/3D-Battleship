a00c9d11f1f9bfde1d0e1bb315d74643b9c1013a
Jaxsen Honeycutt <jaxsen@jxsen.com>
2026-04-15 18:40:50 -0400

    ui disappears when going to war

---
+++ Assets/Scripts/UI/ReadyUI.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Network;
+using UnityEngine;
+
+namespace UI
+{
+    public class ReadyUI : MonoBehaviour
+    {
+        private SessionManager _network;
+
+        private void Start()
+        {
+            _network = FindFirstObjectByType<SessionManager>();
+            if (_network)
+            {
+                _network.OnStateChanged += OnStateChanged;
+            }
+            else
+            {
+                Debug.LogError("Unable to find SessionManager from ReadyUI");
+            }
+        }
+
+        private void OnStateChanged(string state)
+        {
+            if(state=="AtWar") gameObject.SetActive(false);
+        }
+
+        private void OnDestroy()
+        {
+            if (_network) _network.OnStateChanged -= OnStateChanged;
+        }
+    }
+}
